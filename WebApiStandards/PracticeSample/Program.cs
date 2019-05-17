7cbca89a524ea45323c6cbe29857412187c98ae8
shatrudra Pandey <shatrudra.pandey@nagarro.com>
2019-05-17 15:24:49 +0530

    Add new console project for  R&D perpose

---
+++ WebApiStandards/PracticeSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace PracticeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Entry!");
+
+            //TaskProblem.SayHello();
+
+            //Console.WriteLine("Exit!");
+
+            //Thread.Sleep(10000);
+
+
+            //2.0
+
+            Console.WriteLine("Entry!");
+
+            Thread thread = new Thread(new ThreadStart(TaskProblem.SayHello));
+
+            thread.Start();
+
+            Console.WriteLine("Exit!");
+
+            Console.Read();
+        }
+    }
+}
---
+++ WebApiStandards/PracticeSample/TaskProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace PracticeSample
+{
+    public static class TaskProblem
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("SayHello 1");
+
+            Task task = new Task(() => { Console.WriteLine("SayHello 3"); Thread.Sleep(5000); });
+
+            Console.WriteLine("SayHello 2");
+
+            task.Start();
+
+           
+            Console.WriteLine("SayHello 4");
+
+        }
+
+        public static void SayHello2()
+        {
+            Console.WriteLine("SayHello 1");
+
+            Task task = new Task(() => { Console.WriteLine("SayHello 3"); Thread.Sleep(5000); });
+
+            Console.WriteLine("SayHello 2");
+
+            task.Start();
+
+
+            Console.WriteLine("SayHello 4");
+
+        }
+    }
+}
